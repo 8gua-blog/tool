7f9bb5fe42d6b4f83ab6d58456a0d6d16eaa7599
8gua <i@8gua.blog>
2018-01-17 22:41:10 +0800

    .

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,4 +1,7 @@
+
+if [ ! -f "/usr/local/bin/npm" ]; then
 if [ ! -f "/usr/local/bin/brew" ]; then
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 brew install node
+fi
