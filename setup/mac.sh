b777980564a3a40d06e0158e7ec913ddb6fab9ab
8gua <i@8gua.blog>
2018-01-18 13:23:19 +0800

    .

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,7 +1,28 @@
+#!/usr/bin/env bash
 
-if [ ! -f "/usr/local/bin/npm" ]; then
-if [ ! -f "/usr/local/bin/brew" ]; then
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+
+if ! [ -x "$(command -v brew)" ]; then
+    echo "安装 brew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+cd "$(brew --repo)"
+git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+
+cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+
+brew update
 brew install node
+brew upgrade node
+
+if ! [ -x "$(command -v gcc)" ]; then
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+  grep "\*.*Command Line" |
+  head -n 1 | awk -F"*" '{print $2}' |
+  sed -e 's/^ *//' |
+  tr -d '\n')
+softwareupdate -i "$PROD" --verbose;
 fi
