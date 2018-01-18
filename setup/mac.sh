7969475175023c19eec0a9020ba0bf2f02a4f8d6
8gua <i@8gua.blog>
2018-01-18 14:19:22 +0800

    .

--- setup/mac.sh
+++ setup/mac.sh
@@ -1,28 +1,29 @@
 #!/usr/bin/env bash
+# touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+# PROD=$(softwareupdate -l |
+#   grep "\*.*Command Line" |
+#   tail -n 1 | awk -F"*" '{print $2}' |
+#   sed -e 's/^ *//' |
+#   tr -d '\n')
+# softwareupdate -i "$PROD" --verbose;
 
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+# export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
 
-if ! [ -x "$(command -v brew)" ]; then
-    echo "安装 brew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
+# if ! [ -x "$(command -v brew)" ]; then
+#     echo "安装 brew"
+#     ruby \
+# -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" \
+# </dev/null
+# fi
 
-cd "$(brew --repo)"
-git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
+# cd "$(brew --repo)"
+# git remote set-url origin https://mirrors.ustc.edu.cn/brew.git
 
-cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
-git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
+# cd "$(brew --repo)/Library/Taps/homebrew/homebrew-core"
+# git remote set-url origin https://mirrors.ustc.edu.cn/homebrew-core.git
 
-brew update
-brew install node
-brew upgrade node
+# brew update
+# brew install node
+# brew upgrade node
+npm install -g 8gua
 
-if ! [ -x "$(command -v gcc)" ]; then
-touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
-PROD=$(softwareupdate -l |
-  grep "\*.*Command Line" |
-  head -n 1 | awk -F"*" '{print $2}' |
-  sed -e 's/^ *//' |
-  tr -d '\n')
-softwareupdate -i "$PROD" --verbose;
-fi
